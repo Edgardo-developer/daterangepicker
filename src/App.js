407d4352e25e51c6be0c163875c32f9cdeb3a562
Edgar <khachaturovedgar01@gmail.com>
2023-10-04 19:31:07 +0400

    created the input elementary template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const {inInput, setInInput} = useState('MM/DD/YYYY');
+  const {OutInput, setOutInput} = useState('MM/DD/YYYY');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="originInputs">
+          <input type="date" id='pickInOriginal' className='originInput' />
+          <input type="date" id='pickOutOriginal' className='originInput' />
+        </div>
+        <div className="visualInputs">
+          <div id="pickInVisual" className="visualInput">
+            <input type="text" placeholder="MM/DD/YYYY" inputMode="text" value="MM/DD/YYYY" />
+          </div>
+          <div id="pickOutVisual" className="visualInput">
+            <input type="text" placeholder="MM/DD/YYYY" inputMode="text" value="MM/DD/YYYY" />
+          </div>
+        </div>
       </header>
     </div>
   );
